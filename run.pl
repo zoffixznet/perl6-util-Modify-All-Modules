d8d94d31a5016fa534274277e383948c66e58381
Zoffix Znet <cpan@zoffix.com>
2015-04-01 19:49:53 -0400

    Working version

--- run.pl
+++ run.pl
@@ -3,10 +3,13 @@
 use strict;
 use warnings;
 use 5.020;
+use File::Find::Rule;
+use File::Path qw/remove_tree/;
 
 use Mojo::UserAgent;
 use JSON::MaybeXS;
 my $MY_USER = 'zoffixznet';
+my $START_AT = shift;
 
 my $modules = decode_json(
     Mojo::UserAgent->new->get("http://modules.perl6.org/proto.json")->res->body
@@ -17,42 +20,68 @@ my @wanted = map +{
     repo => $modules->{$_}{url},
 }, grep $modules->{$_}{badge_panda_nos11}, keys %$modules;
 
-mkdir "working_dir" or die $!;
-chdir "working_dir" or die "Failed to chdir to working dir";
 for ( sort { $a->{name} cmp $b->{name} } @wanted ) {
+    if ( $START_AT ) {
+        $_->{name} eq $START_AT or next;
+        undef $START_AT;
+    }
+    say "Working on $_->{name}";
+
+    mkdir "working_dir" or die $!;
+    chdir "working_dir" or die "Failed to chdir to working dir";
+
     say "Opening palemoon to take a look at $_->{repo}";
     `palemoon $_->{repo}`;
     say "Do we need to make changes? [Y,n]:";
     chomp( my $answer = <> );
-    next if $answer =~ /n/i;
+    if ( $answer =~ /n/i ) {
+        chdir '..';
+        say "Cleaning up";
+        say "Deleted: " . remove_tree 'working_dir' ;
+        next;
+    }
 
     my ( $owner, $repo) = $_->{repo}
     =~ m{https://github.com/([^/]+)/([^/]+)/};
     say "Forking $owner/$repo";
     say `ph fork $owner/$repo`;
+    sleep 1; # seems sometimes there's a delay on github and cloning fails
     say "Cloning the fork";
     say `git clone https://github.com/$MY_USER/$repo .`;
     say `git checkout -b add_meta_provides`;
-    say `find . -iname '*.pm'`;
-    say `find . -iname '*.pm6'`;
-    say `find . -iname '*.p6'`;
+
+    my @files = File::Find::Rule->file()
+                            ->name(qw/*.pm *.p6 *.pm6/)
+                            ->in('.');
+    say "Found: @files";
+
+        say qq#    "provides": {#;
+        if ( @files == 1 ) {
+            say qq#        "$_->{name}": "$files[0]"#;
+        }
+        else {
+            my $name = $_->{name};
+            for ( @files ) {
+                my $lib = s{^lib/|\.(pm|p6|pm6)}{}gr;
+                $lib =~ s{/}{::}g;
+                say qq#        "$lib": "$_",#;
+            }
+        }
+        say qq#    },#;
+
     say "MODIFY META.info. I'm waiting. Press ENTER to proceed";
     <>;
     say `git commit -am "add provides section for latest panda (S11 support)"`;
     say `git push origin add_meta_provides`;
     `palemoon https://github.com/$MY_USER/$repo`;
-    say qq#    "provides": { "$_->{name}" : "" },#;
     say "We're done with this repo. When you're ready for the next one"
         . " press ENTER\n\n";
-    <>
-}
+    <>;
 
-END {
     chdir '..';
     say "Cleaning up";
-    say `rm -fr working_dir`;
+    say "Deleted: " . remove_tree 'working_dir' ;
 }
-
 
 __END__
 
